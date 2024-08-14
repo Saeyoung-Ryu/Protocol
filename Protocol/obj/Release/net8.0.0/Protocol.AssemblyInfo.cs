ae4fc5c7e4831b50ac30efbde70f0e12ba6dccac
유세영 <usaeyoung9514@gmail.com>
2024-08-14 19:14:56 +0900

    814

--- Protocol/obj/Release/net8.0.0/Protocol.AssemblyInfo.cs
+++ Protocol/obj/Release/net8.0.0/Protocol.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Protocol")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fae1c1ff0187bee90679ba7c73fff763e1d2e5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+462104ce26ac99defd6e18a734c2298a4ddcf407")]
 [assembly: System.Reflection.AssemblyProductAttribute("Protocol")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Protocol")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
